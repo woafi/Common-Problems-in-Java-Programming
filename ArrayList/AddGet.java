0ca1e58c283aa306a807c44c1c6ff46d6ed4b897
Woafi <woafisun@yahoo.com>
2023-11-28 06:18:04 +0600

    3rd commit

---
+++ ArrayList/AddGet.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class AddGet {
+    public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<Integer>();
+
+        //add elements
+        list.add(0);
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        
+        System.out.println(list);
+
+        //get elememts
+        int element =list.get(3);
+        System.out.println(element);
+
+        //add element in between
+
+        list.add(1, 5);
+        System.out.println(list);
+
+        //set element
+        list.set(0,4);
+        System.out.println(list);
+
+        //delete element
+        list.remove(0);
+        System.out.println(list);
+
+        //size
+        System.out.println(list.size());
+        
+        //loops
+        for (int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+            
+        }
+
+        //sorting
+        Collections.sort(list);
+        System.out.println("\n"+list);
+
+
+    }
+    
+}
