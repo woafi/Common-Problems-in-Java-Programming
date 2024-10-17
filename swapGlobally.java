b8b3dd3a15facdd5ff8f636cf69233fa036bec16
Mohammad Woafi <woafisun@yahoo.com>
2024-10-17 22:58:24 +0600

    new file

---
+++ swapGlobally.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	
+static int a, b;
+
+public static void swapf(){
+		int tmp = a;
+		a = b;
+		b = tmp;	
+		return;
+}
+	
+	public static void main(String[] args) {
+		a = 4;
+		b = 3;
+		swapf();
+		System.out.println(a+" "+b);
+	}
+}
